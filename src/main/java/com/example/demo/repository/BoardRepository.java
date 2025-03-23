fb7a17937d8b776cf7076abe3b1bbe87c520cbc3
K <129897371+CinnamonF@users.noreply.github.com>
2025-03-24 03:21:39 +0900

    Delete src directory

--- src/main/java/com/example/demo/repository/BoardRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.Board;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-// JpaRepository를 상속받으면 기본적인 CRUD 기능을 자동으로 제공
-public interface BoardRepository extends JpaRepository<Board, Long> {
-}
